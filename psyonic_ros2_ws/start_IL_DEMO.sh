c4b1bbb84cde50ca62a722c043fca1a22ec8ccaf
sgossett <sgossett2020@gmail.com>
2026-02-13 13:41:29 -0800

    cleaned ros2 and added requirements

--- psyonic_ros2_ws/start_IL_DEMO.sh
+++
@@ -1,6 +0,0 @@
-source /opt/ros/jazzy/setup.bash
-source IL_ROS_env/bin/activate
-export PYTHONPATH=$PYTHONPATH:$(pwd)/IL_ROS_env/lib/python3.12/site-packages
-source install/setup.bash
-cd launch
-ros2 launch IL_demo_launch.py
---
+++ psyonic_ros2_ws/start_IL_DEMO_ROS.sh
@@ -0,0 +1,6 @@
+source /opt/ros/jazzy/setup.bash
+source IL_ROS_venv/bin/activate
+export PYTHONPATH=$PYTHONPATH:$(pwd)/IL_ROS_venv/lib/python3.12/site-packages
+source install/setup.bash
+cd launch
+ros2 launch IL_demo_launch.py

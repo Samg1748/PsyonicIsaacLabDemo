84e24a19b9accab18eb59a6ef8f58a8187543d12
sgossett <sgossett2020@gmail.com>
2026-02-27 14:12:30 -0800

    added comments to help real2sim

--- psyonic_ros2_ws/src/real2sim/real2sim/real2sim_script.py
+++ psyonic_ros2_ws/src/real2sim/real2sim/real2sim_script.py
@@ -10,47 +10,74 @@ import threading
 import time
 
 class Real2SimNode(Node):
+    """
+    Handles ROS2 Node and Publishing of UR control
+        has 2 threads: robot control thread and ros2 thread
+    """
     def __init__(self):
         super().__init__('Psyonic_Real2Sim')
+
+        # initializes UR_RTDE
         self.rtde_c = rtde_control.RTDEControlInterface("192.168.1.10")
         self.rtde_r = rtde_receive.RTDEReceiveInterface("192.168.1.10")
+        
+        # initializes ABH client
         self.client = AHSerialClient()
 
+        # initializes lock for shared variables (to prevent race conditions)
         self.lock = threading.Lock()
+
+        # start pose of hand and arm (can be changed)
         start_arm = [-91 * (3.14/180), -80 * (3.14/180), 107 * (3.14/180), -32 * (3.14/180), 90 * (3.14/180), -100 * (3.14/180)]
         hand_open = [0, 0, 0, 0, 10, -100]
         self.latest_robot_position = start_arm + hand_open
+        
+        # boolean to for joining threads if running stops
         self.running = True
+
+        # qos profile to help with faster polling
         qos_profile_best_effort = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             durability=DurabilityPolicy.VOLATILE, # Volatile is typical for best effort according to internet
             depth=1 
         )
+
+        # initializes publisher to topic Psyonic_Real2Sim_Topic
         self.publisher = self.create_publisher(JointState, 'Psyonic_Real2Sim_Topic', qos_profile_best_effort)
         self.pub_timer = self.create_timer(0.05, self.pub_callback)
 
+        # Starts robot control thread (doing robot_control_loop)
         self.control_thread = threading.Thread(target=self.robot_control_loop)
         self.control_thread.daemon = True
         self.control_thread.start()
         
 
     def pub_callback(self):
+        # initializes msg
         self.msg = JointState()
         self.msg.name = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint', 'index_q1', 'middle_q1', 'ring_q1', 'pinky_q1', 'thumb_q2', 'thumb_q1']
+        # gets UR_RTDE arm values
         arm_command = self.rtde_r.getActualQ()
+        # gets ability hand values
         hand_command = self.client.hand.get_position()
         obs = arm_command + hand_command
+        
+        # with lock on shared variables, publish message to topic (might not need this lock TODO)
         with self.lock:
             self.latest_robot_position = obs
         self.msg.position = self.latest_robot_position
         self.publisher.publish(self.msg)
 
     def send_robot_action(self, arm_position, hand_position):
+        # with lock on shared variables save to latest_position variable to send command
         with self.lock:
             self.latest_robot_position = arm_position + hand_position
         
+        # send move command to UR arm
         self.rtde_c.moveJ(arm_position, 2.0, 1.4) #1.05 1.4
+        # send position command to ability hand
         self.client.set_position(hand_position)
+        # slight wait to prevent resource loop with no way out
         time.sleep(0.05)
 
 
@@ -88,7 +115,7 @@ class Real2SimNode(Node):
             self.send_robot_action(cup_flipped_down_pos, hand_open)
             
             self.send_robot_action(arm_flipped_back_pos, hand_open)
- ###########################
+ ########################### end of series, not reverse out
             self.send_robot_action(arm_flipped_back_pos, hand_open)
 
             self.send_robot_action(cup_flipped_down_pos, hand_open)
@@ -107,6 +134,8 @@ class Real2SimNode(Node):
 
 
     def destroy_node(self):
+
+        # destroys node when stopped
         self.running = False
         self.control_thread.join()
         super().destroy_node()
--- psyonic_scripts/real2sim.py
+++ psyonic_scripts/real2sim.py
@@ -1,5 +1,7 @@
 from isaaclab.app import AppLauncher
-
+"""
+Starts IsaacLab App. Needs to be above everything else
+"""
 app_launcher = AppLauncher()
 simulation_app = app_launcher.app
 
@@ -41,7 +43,10 @@ import torch
 
 @configclass
 class ActionsCfg:
-    # 1. Hand remains as standard joint positions
+    """
+    Actions in ManagerBasedEnv
+    """
+    # Joint actions no offset needed
     hand_joint_positions = mdp.JointPositionActionCfg(
         asset_name="robot", 
         use_default_offset=False, 
@@ -51,9 +56,15 @@ class ActionsCfg:
 
 @configclass
 class ObservationsCfg:
+    """
+    Observations for ManagerBasedEnv
+    """
 
     @configclass
     class RobotObsCfg(ObsGroup):
+        """
+        Observes joint positions of Robot
+        """
         joint_pos = ObsTerm(func=mdp.joint_pos)
 
     Robot_obs: RobotObsCfg = RobotObsCfg()
@@ -61,6 +72,9 @@ class ObservationsCfg:
 
     @configclass
     class LastAction(ObsGroup):
+        """
+        Observes last action performed by Robot
+        """
         last_action = ObsTerm(func=mdp.last_action)
     
     Last_action: LastAction = LastAction()
@@ -69,6 +83,11 @@ class ObservationsCfg:
 
 @configclass
 class EventCfg:
+    """
+    Events for ManagerBasedEnv
+    """
+
+    #resets Robot joints when called
     reset_cart_position = EventTerm(
         func=mdp.reset_joints_by_offset,
         mode="reset",
@@ -82,14 +101,21 @@ class EventCfg:
 
 @configclass
 class PsyonicURSceneCfg(InteractiveSceneCfg):
+    """
+    Interactive Scene for Psyonic Robot
+    """
 
+    # Initializes the Articulation of psyonic robot
     robot: ArticulationCfg = ArticulationCfg(
         prim_path="{ENV_REGEX_NS}/Robot", 
         spawn=sim_utils.UsdFileCfg(
             usd_path="USD_assets/psyonic_UR_right_playground.usd", ##############################
         ),
+        # initial position of robot
         init_state=ArticulationCfg.InitialStateCfg(
+            #root position
             pos=(0.0, 0.0, 0.035),
+            # joint positions
             joint_pos={
                 "shoulder_pan_joint": -0.2, 
                 "shoulder_lift_joint": -1.27, 
@@ -105,6 +131,7 @@ class PsyonicURSceneCfg(InteractiveSceneCfg):
                 "thumb_q2": 0.0
             }
         ),
+        # initializaes actuators of Robot
         actuators={
             "shoulder": ImplicitActuatorCfg(
                                         joint_names_expr=["shoulder_.*"],
@@ -138,7 +165,7 @@ class PsyonicURSceneCfg(InteractiveSceneCfg):
         }
     )
 
-
+    # Add frame for center of the hand (used for data collection)
     center_hand: FrameTransformerCfg = FrameTransformerCfg(
     prim_path="{ENV_REGEX_NS}/Robot/ur5e/wrist_3_link",
     target_frames=[
@@ -151,12 +178,13 @@ class PsyonicURSceneCfg(InteractiveSceneCfg):
         ),
     ],
 )
-
+    # Initializes the ground floot
     ground = AssetBaseCfg(
         prim_path="/World/ground",
         spawn=sim_utils.GroundPlaneCfg(),
     )
 
+    # Initializes the lights of scene
     dome_light = AssetBaseCfg(
         prim_path="/World/dome_light",
         spawn=sim_utils.DomeLightCfg(intensity=3000.0),
@@ -165,57 +193,82 @@ class PsyonicURSceneCfg(InteractiveSceneCfg):
 
 @configclass
 class PickupCupCfg(ManagerBasedEnvCfg):
+    """
+    Sets up the ManagerBasedEnv for the Interactive Scene
+    """
 
+    # Initializes interactive scene
     scene = PsyonicURSceneCfg(num_envs=1, env_spacing=2.0)
 
+    # Initializes observations, actions, and events in ManagerBasedEnv
     observations = ObservationsCfg()
     actions = ActionsCfg()
     events = EventCfg()
 
+    # Controls the viewing angle and simulation settings
     def __post_init__(self):
-    # viewer settings
-    
+        
+        # viewer settings
         self.viewer.eye = [1.25, 0.0, 1.75]
         self.viewer.lookat = [0.35, 0.0, 0.0]
 
         # step settings
-        self.decimation = 1  # env step every 4 sim steps: 200Hz / 4 = 50Hz
+        self.decimation = 1  # env step ex: every 4 sim steps: (200Hz / 4 = 50Hz)
         # simulation settings
         self.sim.dt = 0.05  # sim step every 5ms: 200Hz
 
 
 class PsyonicNode(Node):
+    """
+    Manages the ROS2 Node for Joint State Subscriptions
+    """
     def __init__(self):
         super().__init__('psyonic_node')
         
+        # qos profile for volatile data leading to faster polling with queue of 1 to get latest reading only
         qos_profile_best_effort = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             durability=DurabilityPolicy.VOLATILE, # Volatile is typical for best effort according to internet
             depth=1
         )
+
+        # creates subscriber object to topic Psyonic_Real2Sim_Topic
         self.subscriber = self.create_subscription(JointState, 'Psyonic_Real2Sim_Topic', self.sim_callback, qos_profile_best_effort)
+        
+        # prevent non-use error (useless)
         self.subscriber
 
+        # initializes MangerBasedEnv
         env_cfg = PickupCupCfg()
+
+        # plugs in config
         self.env = ManagerBasedEnv(cfg=env_cfg)
+        
+        # joint names of msg
         self.msg_names = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint', 'index_q1', 'middle_q1', 'ring_q1', 'pinky_q1', 'thumb_q1','index_q2', 'middle_q2', 'ring_q2', 'pinky_q2', 'thumb_q2']
         
-
+        # initializes robot into class variable
         self.robot = self.env.scene["robot"]
+
+        # initializes actions to speed up allocation
         actions = torch.zeros(self.env.num_envs, self.env.action_manager.total_action_dim, device=self.env.device)
 
-
+        # initial reset of IL environment
         self.env.reset()
 
-
+    # callback for each subscription msg
     def sim_callback(self, msg):
         self.msg = msg
+
+        # offsets for arm position if needed
         self.msg.position[0] += 1.5708 #####ADD ARM OFFSETS HERE!!!
         # self.msg.position[1] += 0.0 #####ADD ARM OFFSETS HERE!!!
         # self.msg.position[2] += 0.0 #####ADD ARM OFFSETS HERE!!!
         # self.msg.position[3] += 0.0 #####ADD ARM OFFSETS HERE!!!
         # self.msg.position[4] += 0.0 #####ADD ARM OFFSETS HERE!!!
         # self.msg.position[5] += 0.0 #####ADD ARM OFFSETS HERE!!!
+
+        # converts psyonic finger position into degrees then radians (messy TODO)
         self.msg.position[6] *= (3.14/180/1.1)
         self.msg.position[7] *= (3.14/180/1.1)
         self.msg.position[8] *= (3.14/180/1.1)
@@ -223,18 +276,33 @@ class PsyonicNode(Node):
         self.msg.position[10] *= (3.14/180/1.1)
         self.msg.position[11] *= (3.14/180/1.1)
         self.msg.position[10], self.msg.position[11] = self.msg.position[11], self.msg.position[10]
+        
+        # logs msg for debugging purposes
         self.get_logger().info(f'recieved: {self.msg.position}')
+        
+        # transfers msg into a tensor
         action = torch.tensor(msg.position, device=self.env.device)
+        
+        # creates appropriate action space if multiple environments
         actions = action.repeat(self.env.num_envs, 1)
+
+        # steps through environment with desired action
         obs, _ = self.env.step(actions)
 
 
     
 def main():
+    
+    # initializes ros2
     ros2.init()
+
+    # initializes ros2 node
     psy_sub = PsyonicNode()
 
+    # spin indefinitely
     ros2.spin(psy_sub)
+
+    # after interrupt, destroy node for clean exit
     psy_sub.destroy_node()
     ros2.shutdown()
 

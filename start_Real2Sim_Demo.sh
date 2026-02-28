877cae41f8d3ff09e059d434a0213faa50ca662a
Sam G <sgossett2020@gmail.com>
2026-02-27 23:36:17 -0500

    fixed start scripts

--- start_Real2Sim_Demo.sh
+++ start_Real2Sim_Demo.sh
@@ -2,6 +2,6 @@ sudo chmod 666 /dev/ttyUSB0
 
 export ROS_DISTRO=jazzy
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/user/miniconda3/envs/isaaclab_env/lib/python3.11/site-packages/isaacsim/exts/isaacsim.ros2.bridge/jazzy/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/miniconda3/envs/isaaclab_env/lib/python3.11/site-packages/isaacsim/exts/isaacsim.ros2.bridge/jazzy/lib
 
 ./../IsaacLab/isaaclab.sh -p psyonic_scripts/real2sim.py

0fe48a53b91eb21318748fa5acaa35ff380a4465
sgossett <sgossett2020@gmail.com>
2026-03-03 14:30:00 -0800

    working on haply and manus integration - still wip

--- psyonic_ros2_ws/src/manus_haply_teleop/manus_haply_teleop/inverse3_manus_pub.py
+++ psyonic_ros2_ws/src/manus_haply_teleop/manus_haply_teleop/inverse3_manus_pub.py
@@ -3,6 +3,10 @@ from rclpy.node import Node
 from sensor_msgs.msg import JointState
 from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy
 
+from rclpy.executors import MultiThreadedExecutor
+import threading
+import time
+
 class Inverse3ManusPubisher(Node):
     def __init__(self):
         super().__init__("Inverse3ManusPub")
@@ -12,27 +16,66 @@ class Inverse3ManusPubisher(Node):
                     depth=1 
                 )
         self.publisher = self.create_publisher(JointState, 'Inverse3Manus_Topic', qos_profile_best_effort)
-        self.pub_timer = self.create_timer(0.05, self.teleop_pub_callback)
-        self.count = 0.0
+        self.sub_manus = self.create_subscription(ManusGlove, 'manus_glove_0', self.teleop_manus_callback, qos_profile_best_effort)
+        
+        self.pub_msg = JointState()
+        self.pub_msg.name = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint', 'index_q1', 'middle_q1', 'ring_q1', 'pinky_q1', 'thumb_q2', 'thumb_q1']
 
-    def teleop_pub_callback(self):
-        self.msg = JointState()
-        self.msg.name = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint', 'index_q1', 'middle_q1', 'ring_q1', 'pinky_q1', 'thumb_q2', 'thumb_q1']
-        # get values of Inverse3 and Manus gloves
+        self.manus_latest = list(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        self.haply_latest = list(0.0, 0.0, 0.0, 0.0, 0.0, 0.0) #CHECK THISSS
 
-        # formats to Inverse_Manus_Topic
+        self.lock = threading.lock()
+        self.running = True
+
+        self.haply_thread = threading.Thread(target=self.teleop_haply)
+        self.haply_thread.daemon = True
+        self.haply_thread.start()
+
+
+    def teleop_manus_callback(self, msg):
+        # gets manus reading
+        msg
+        # formats manus reading to store in latest var
+        self.manus_latest
+
+        # gets haply latest
+        with self.lock:
+            self.pub_msg.position = self.manus_latest + self.haply_latest
 
         # publishes to Inverse_Manus_Topic
-        self.msg.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, self.count, self.count/2, self.count, 0.0, 0.0, -self.count]
-        self.count += 0.05
-        self.publisher.publish(self.msg)
+        self.pub_msg.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.publisher.publish(self.pub_msg)
+        pass
+
+    def teleop_haply(self):
+
+        # while node is active, thread runs
+        while self.running and ros2.ok():
+            pass
+        # gets haply reading
+
+        # formats reading to store in latest var
+        self.haply_latest
+        pass
+        
+    def destroy_node(self):
+        self.running= False
+        self.haply_thread.join()
+        super().destroy_node()
 
 
 def main():
     ros2.init()
     pub = Inverse3ManusPubisher()
 
-    ros2.spin(pub)
+    executor = MultiThreadedExecutor()
+    executor.add_node(pub)
+
+    try:
+        ros2.spin(pub)
+    except KeyboardInterrupt:
+        pass
+
 
     pub.destroy_node()
     ros2.shutdown()
